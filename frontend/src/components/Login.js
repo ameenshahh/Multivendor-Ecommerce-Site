d17b2d79f7b35e91d46004e2b236d6cb8d802b64
Ameen Shah <ameenshahshahjahan@gmail.com>
2023-05-01 12:29:04 +0530

    demo change to test git config works

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      Loginnnnn
+    </div>
+  );
+};
+
+export default Login;
